e6ebf69d99f92be4745f6c1c6aeac279595960a2
Hanpla <hanpla25@gmail.com>
2026-01-25 13:18:12 +0900

    feat: AuthBtns 컴포넌트 추가

---
+++ components/auth/AuthBtns.tsx
@@ -0,0 +1,10 @@
+export default function AuthLoginBtn() {
+  return (
+    <button
+      type="submit"
+      className="w-full bg-neutral-900 text-white font-bold py-4 rounded-2xl hover:bg-neutral-800 active:scale-[0.98] transition-all"
+    >
+      로그인하기
+    </button>
+  );
+}
