844441d16d9074842f18183dbe0b1e4b8b8b1401
Hanpla <hanpla25@gmail.com>
2026-01-25 12:59:23 +0900

    feat: PageTitle 컴포넌트 추가

---
+++ components/common/TitleText.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  href?: string;
+}
+
+export default function PageTitle({ title, href }: Props) {
+  const baseStyle = `font-bold text-neutral-800`;
+
+  if (href) {
+    return (
+      <Link href={href} className="inline-block">
+        <h1 className={`${baseStyle}`}>{title}</h1>
+      </Link>
+    );
+  }
+
+  return <h1 className={baseStyle}>{title}</h1>;
+}
