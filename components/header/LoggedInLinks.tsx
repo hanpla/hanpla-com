164a83fcd56ff04a7365a567b4732260923e9a6d
Hanpla <hanpla25@gmail.com>
2026-01-29 11:08:12 +0900

    chore: 로그아웃 추가

--- components/header/LoggedInLinks.tsx
+++ components/header/LoggedInLinks.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
+// Components
+import LogoutBtn from "../common/LogoutBtn";
+
 export default function LoggedInLinks() {
   return (
     <>
       <Link href="/board" className="hidden md:block text-sm text-neutral-800">
         전체 게시판
       </Link>
-      <button className="hidden md:block text-sm text-neutral-800 cursor-pointer">
-        로그아웃
-      </button>
+      <LogoutBtn className="hidden md:block text-sm" />
     </>
   );
 }
