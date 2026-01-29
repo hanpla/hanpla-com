f81d581f0752024f11d3cd7f1b032029dd7e0a31
Hanpla <hanpla25@gmail.com>
2026-01-29 15:09:53 +0900

    chore: callbackUrl 함수 분리

--- components/header/LoggedOutLinks.tsx
+++ components/header/LoggedOutLinks.tsx
@@ -3,20 +3,18 @@
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 
+// Utils
+import { generateCallbackUrl } from "@/libs/utils/utils";
+
 export default function LoggedOutLinks() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const fullPath = searchParams.toString()
-    ? `${pathname}?${searchParams.toString()}`
-    : pathname;
-
-  const redirectPath =
-    pathname === "/login" || pathname === "/signup" ? "/" : fullPath;
+  const callbackUrl = generateCallbackUrl(pathname, searchParams.toString());
 
   const AUTH_LINKS = [
     { label: "전체 게시판", href: "/board" },
-    { label: "로그인", href: `/login?callbackUrl=${redirectPath}` },
+    { label: "로그인", href: `/login?callbackUrl=${callbackUrl}` },
     { label: "회원가입", href: "/signup" },
   ];
   return (
