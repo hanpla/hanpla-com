78d8d96ecb6fabc558ef4f6692599c7ad6993681
Hanpla <hanpla25@gmail.com>
2026-01-29 09:54:24 +0900

    chore: Search 컴포넌트 내부 버튼 분리

--- components/header/Search.tsx
+++ components/header/Search.tsx
@@ -17,28 +17,43 @@ interface Props {
 
 export default function Search({ allBoards }: Props) {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const openSearch = () => setIsSearchOpen(true);
+  const closeSearch = () => setIsSearchOpen(false);
 
   return (
     <>
-      <button
-        type="button"
-        className="hidden md:flex items-center justify-center bg-neutral-100 px-2 py-1 rounded-lg gap-6 cursor-pointer hover:bg-neutral-200 transition-colors"
-        onClick={() => setIsSearchOpen(true)}
-      >
-        <span className="text-neutral-400 text-[14px]">전체 게시판 검색</span>
-        <SearchIcon size={20} className="text-neutral-600 p-0.5" />
-      </button>
-
-      <button type="button" className="md:hidden p-2 text-neutral-600">
-        <SearchIcon size={20} onClick={() => setIsSearchOpen(true)} />
-      </button>
+      <SearchBtn onClick={openSearch} />
+      <SearchBtn onClick={openSearch} isMobile />
 
       {isSearchOpen && (
-        <SearchBoardModal
-          allBoards={allBoards}
-          onClose={() => setIsSearchOpen(false)}
-        />
+        <SearchBoardModal allBoards={allBoards} onClose={closeSearch} />
       )}
     </>
   );
 }
+
+const SearchBtn = ({
+  onClick,
+  isMobile = false,
+}: {
+  onClick: () => void;
+  isMobile?: boolean;
+}) => {
+  if (isMobile) {
+    return (
+      <button onClick={onClick} className="md:hidden p-2 text-neutral-600">
+        <SearchIcon size={20} />
+      </button>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="hidden md:flex items-center justify-center bg-neutral-100 px-2 py-1 rounded-lg gap-6 cursor-pointer hover:bg-neutral-200 transition-colors"
+    >
+      <span className="text-neutral-400 text-[14px]">전체 게시판 검색</span>
+      <SearchIcon size={20} />
+    </button>
+  );
+};
