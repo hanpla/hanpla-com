dd291b3bd0d1fe6bc7015a8e00bb4b59ff6265e0
Hanpla <hanpla25@gmail.com>
2026-02-01 13:49:11 +0900

    feat: 회원가입 함수 구현

--- libs/actions/auth.ts
+++ libs/actions/auth.ts
@@ -55,18 +55,64 @@ export async function loginAction(
     await createSession(userData);
   } catch (error) {
     console.error("서버 내부 에러:", error);
-    return {
-      success: false,
-      message: "오류가 발생했습니다. 다시 시도해주세요.",
-      inputs: { userId: userId },
-    };
+    return createAuthError(
+      "오류가 발생했습니다. 다시 시도해주세요.",
+      currentInputs,
+    );
   }
 
   redirect(getSafeRedirect(callbackUrl));
 }
 
-export async function logoutAction(callbackUrl: string) {
-  await deleteSession();
+export async function signupAction(
+  _prevState: AuthState,
+  formData: FormData,
+): Promise<AuthState> {
+  const userNickname = formData.get("userNickname") as string;
+  const userId = formData.get("userId") as string;
+  const userPassword = formData.get("userPassword") as string;
 
-  redirect(getSafeRedirect(callbackUrl));
+  const currentInputs = { userId, userNickname, userPassword };
+
+  try {
+    const supabase = await createClient();
+    const { data: existingUser } = await supabase
+      .from("users")
+      .select("user_id, nickname")
+      .or(`user_id.eq.${userId},nickname.eq.${userNickname}`)
+      .maybeSingle();
+
+    if (existingUser) {
+      const msg = existingUser.user_id === userId ? "아이디" : "닉네임";
+      return createAuthError(`이미 사용중인${msg}입니다.`, currentInputs);
+    }
+
+    const { error: insertError } = await supabase.from("users").insert([
+      {
+        user_id: userId,
+        password: userPassword,
+        nickname: userNickname,
+        role: "user",
+      },
+    ]);
+
+    if (insertError) {
+      console.log(insertError);
+      return createAuthError(
+        "오류가 발생했습니다. 다시 시도해주세요.",
+        currentInputs,
+      );
+    }
+  } catch (error) {
+    console.error("서버 내부 에러:", error);
+    return createAuthError(
+      "오류가 발생했습니다. 다시 시도해주세요.",
+      currentInputs,
+    );
+  }
+  redirect("/login");
+}
+
+export async function logoutAction() {
+  await deleteSession();
 }
