607150d04f441ddc5500be91399042d729827d36
Hanpla <hanpla25@gmail.com>
2026-02-02 20:23:00 +0900

    chore: Post 타입 추가

--- libs/types/board.ts
+++ libs/types/board.ts
@@ -2,3 +2,22 @@ export interface BoardType {
   name: string;
   abbr: string;
 }
+
+export interface PostListType {
+  id: number;
+  title: string;
+  boardName: string;
+  abbr: string;
+  nickname: string;
+  views: number;
+  comments: number;
+  likes: number;
+  isLogin: boolean;
+  ip: string;
+  createdAt: string;
+}
+
+export interface GetPostListResult {
+  postList: PostListType[];
+  totalCount: number;
+}
