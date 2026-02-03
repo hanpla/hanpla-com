402695a487d0a7072d2547df025738b778bd1525
Hanpla <hanpla25@gmail.com>
2026-02-03 09:45:56 +0900

    feat: Home 페이지 인기글 목록 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>안녕하세요</h1>;
+// Actions
+import { getPostList } from "@/libs/actions/board";
+
+// Components
+import AbbrMain from "@/components/abbr";
+import PageTitle from "@/components/common/TitleText";
+
+export default async function Home() {
+  const postListData = await getPostList({
+    abbr: "best",
+    page: 1,
+    limit: 20,
+    likeCount: 0,
+  });
+
+  return (
+    <>
+      <PageTitle title="인기글" href="/" />
+      <AbbrMain postListData={postListData} isBest abbr="best" />
+    </>
+  );
 }
