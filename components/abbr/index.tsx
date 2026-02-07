e15e7264cffac1ac8feaeb4b51f73109942bb5a3
Hanpla <hanpla25@gmail.com>
2026-02-07 09:55:15 +0900

    feat: 페이지네이션 컴포넌트 추가

--- components/abbr/index.tsx
+++ components/abbr/index.tsx
@@ -1,28 +1,38 @@
 // Components
+import Pagination from "../common/Pagination";
 import PostList from "./PostList";
 import Search from "./Search";
 
 // Types
 import { GetPostListResult } from "@/libs/types/board";
 
-// Types
 interface Props {
   postListData: GetPostListResult;
   isBest?: boolean;
   abbr: string;
+  ITEM_PER_PAGE: number;
+  currentPage: number;
 }
 
 export default function AbbrMain({
   postListData,
   isBest = false,
   abbr,
+  ITEM_PER_PAGE,
+  currentPage,
 }: Props) {
   const postList = postListData.postList;
   const totalCount = postListData.totalCount;
+  const totalPage = Math.ceil(totalCount / ITEM_PER_PAGE) || 1;
 
   return (
     <>
       <PostList postList={postList} isBest={isBest} />
+      <Pagination
+        href={`/board/${abbr}`}
+        currentPage={currentPage}
+        totalPage={totalPage}
+      />
       <Search abbr={abbr} />
     </>
   );
