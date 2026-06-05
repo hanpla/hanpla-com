fca63952474e7c0d922dc8e2d54c410a946fb27b
Hanpla <hanpla25@gmail.com>
2026-06-05 20:42:41 +0900

    style: 아이콘 컴포넌트 Props 리팩토링 및 기본값 지원

--- components/icons/MenuIcon.tsx
+++ components/icons/MenuIcon.tsx
@@ -1,11 +1,19 @@
-export default function MenuIcon({ className }: { className?: string }) {
+import { IconProps } from "@/types/icon";
+
+export default function MenuIcon({
+  size = 24,
+  color = "currentColor",
+  className = "",
+}: IconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={2}
-      stroke="currentColor"
+      stroke={color}
+      width={size}
+      height={size}
       className={className}
     >
       <path
@@ -16,3 +24,4 @@ export default function MenuIcon({ className }: { className?: string }) {
     </svg>
   );
 }
+
