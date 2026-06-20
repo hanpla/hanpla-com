44ec154955cd7c704d61ceb2fa5b2a057482b20e
Hanpla <hanpla25@gmail.com>
2026-06-20 10:48:16 +0900

    refactor: Object.groupBy() 표준 함수를 사용하여 게시판 카테고리 그룹화 로직 간소화

--- lib/utils/board.ts
+++ lib/utils/board.ts
@@ -9,16 +9,10 @@ export interface GroupedBoards {
  * 게시판 목록을 카테고리별로 그룹화하고 카테고리 이름을 정렬하여 반환합니다.
  */
 export const groupBoardsByCategory = (boards: Board[]): GroupedBoards => {
-  const groupedBoards = boards.reduce<Record<string, Board[]>>((acc, board) => {
-    const category = board.category || "기타";
-    if (!acc[category]) {
-      acc[category] = [];
-    }
-    acc[category].push(board);
-    return acc;
-  }, {});
-
+  const grouped = Object.groupBy(boards, (board) => board.category || "기타");
+  const groupedBoards = grouped as Record<string, Board[]>;
   const categories = Object.keys(groupedBoards).sort();
 
   return { groupedBoards, categories };
 };
+
