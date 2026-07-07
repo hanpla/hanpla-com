eb67f27be406b6a90d7f6ecd40e750c3c5967975
Hanpla <hanpla25@gmail.com>
2026-07-07 19:06:32 +0900

    chore: 페이지 타이틀 링크 추가

--- app/(main)/boards/[abbr]/page.tsx
+++ app/(main)/boards/[abbr]/page.tsx
@@ -30,7 +30,7 @@ const BoardDetailPage = async ({ params }: BoardDetailPageProps) => {
 
   return (
     <>
-      <PageTitle title={board.name} />
+      <PageTitle title={board.name} href={`/boards/${board.abbr}`} />
       <div className="mt-6 text-sm text-zinc-500 dark:text-zinc-400">
         <p>[{board.abbr}] 게시판 글 목록이 여기에 표시될 예정입니다.</p>
       </div>
